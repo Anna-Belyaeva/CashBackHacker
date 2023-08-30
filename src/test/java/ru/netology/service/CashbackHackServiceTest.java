b8c7d71b508734a32c73ef9645649edf5af4f08f
Anna Belyaeva <anya.belyaeva.1998@list.ru>
2023-08-30 22:08:59 +0300

    Added tests TestNG

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,27 @@
+package ru.netology.service;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldBuy1() {
+
+        Assert.assertEquals(service.remain(999), 1);
+    }
+
+    @Test
+    public void shouldBuy999() {
+
+        Assert.assertEquals(service.remain(1001), 999);
+    }
+
+    @Test
+    public void shouldBuy1000() {
+
+        Assert.assertEquals(service.remain(1000), 1000);
+    }
+
+}
